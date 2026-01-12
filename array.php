7385519b9551007faeba32f495514bd8cf2dd208
vinod sharma <sharmavinodbadboy@gmail.com>
2026-01-12 14:45:47 +0530

    changes

--- array.php
+++ array.php
@@ -1,51 +1,44 @@
 <?php
-// Array of 5 students with scores in 3 subjects
-$students = [
-   "student1" => [
+
+$testscore = [
+    "vinod" => [
         "Math" => 51,
         "Science" => 33,
         "English" => 37
     ],
-  "student2" => [
+    "gaourav" => [
         "Math" => 39,
         "Science" => 44,
         "English" => 32
     ],
-   "student3" => [
+    "sachin" => [
         "Math" => 33,
         "Science" => 46,
         "English" => 40
     ],
-  "student4" =>  [
+    "hardik" =>  [
         "Math" => 48,
         "Science" => 70,
         "English" => 35
     ],
- "student5" =>   [
+    "rohit" =>   [
         "Math" => 37,
         "Science" => 44,
         "English" => 49
     ]
 ];
-// Initialize total scores
 $totalMath = 0;
 $totalScience = 0;
 $totalEnglish = 0;
-$studentCount = count($students);
-
-foreach ($students as $student) {
+$studentCount = count($testscore);
+foreach ($testscore as $student) {
     $totalMath += $student["Math"];
     $totalScience += $student["Science"];
     $totalEnglish += $student["English"];
 }
-
-// Calculate averages
 $averageMath = $totalMath / $studentCount;
 $averageScience = $totalScience / $studentCount;
 $averageEnglish = $totalEnglish / $studentCount;
-
-// Display the results
 echo "Average Math score: " . round($averageMath, 2) . "<br>";
 echo "Average Science score: " . round($averageScience, 2) . "<br>";
 echo "Average English score: " . round($averageEnglish, 2) . "<br>";
-?>
